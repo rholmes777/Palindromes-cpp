70ef4ffce7047ae43a47d3265ac18e95354c1363
Richard Holmes <rholmes@richardholmes.net>
2013-01-10 21:07:16 -0600

    Initial Commit

---
+++ Palindromes/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Palindromes
+//
+//  Created by Richard Holmes on 1/10/13.
+//  Copyright (c) 2013 Richard Holmes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
